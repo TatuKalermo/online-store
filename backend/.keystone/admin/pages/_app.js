951f458b1791cc8a8b411dfa3b81e50f3d758f05
tkalermo <tatu.kalermo@eficode.com>
2021-03-12 16:26:13 +0200

    Adds seed data

--- backend/.keystone/admin/pages/_app.js
+++ backend/.keystone/admin/pages/_app.js
@@ -198,7 +198,7 @@ export default function App({ Component, pageProps }) {
     <Core>
       <KeystoneProvider
         adminConfig={adminConfig}
-        adminMetaHash="3emv5a"
+        adminMetaHash="1t5pbc7"
         fieldViews={fieldViews}
         lazyMetadataQuery={lazyMetadataQuery}
       >
